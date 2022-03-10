252de74b942ba38e65cfbf5f021c672c60e2554b
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-03-09 16:42:01 -0800

    added mysql2 stuff

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+const mysql = require('mysql2');
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// CONNECT TO DATABASE
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL user,
+        user: 'root@localhost',
+        //Your MYSQL pass,
+        password: 'Homebrewed2003!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
